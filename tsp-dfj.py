d04fcd0550362ecf20721126b67f1e6cfa1049d3
Linda BELKESSA <il_belkessa@esi.dz>
2022-11-21 11:14:03 +0100

    update

--- tsp-dfj.py
+++ tsp-dfj.py
@@ -4,71 +4,61 @@ import numpy as np
 import matplotlib.pyplot as plt
 from itertools import chain, combinations
 
+
+from functools import reduce
+from itertools import combinations, product
+
 def solve_tsp():
 
     m = Model(name='tsp', log_output = False)
 
     nb_villes = 5
 
-    sommets = [i for i in range(nb_villes)]
-    arcs = [(i,j) for i in sommets for j in sommets if i!=j]
-
-    #Generate random coordinates
-
-    rnd = np.random
-    rnd.seed(1)
-
-    X = (rnd.rand(nb_villes)*100).round()
-    Y = (rnd.rand(nb_villes)*100).round()
-
-
-    plt.figure(figsize=(10,10))
-    plt.scatter(X,Y,color='purple')
-    s=[]
-
-    for n in range(len(X)):
-        s_temp=[]
-        s_temp.append("%.1f" %X[n])
-        s_temp.append("%.1f" %Y[n])
-        s.append(s_temp)
-
-    plt.xlabel("Distance X")
-    plt.ylabel("Distance Y")
-    plt.title("Dispertion des sommets - TSP")
-
-    for n in range(len(X)):
-        plt.annotate(str(s[n]), xy=(X[n],Y[n] ), xytext=(X[n]-4,Y[n]-4), 
-                    color='green')
-        
-    for n in range(len(X)):
-        plt.annotate(str(n), xy=(X[n],Y[n] ), xytext=(X[n]+0.5,Y[n]+1),
-                    color='red')
-
-    #plt.show()
-
-    distances={(i, j): np.hypot(X[i] - X[j], Y[i] - Y[j]) for i,j in arcs}
-
-    m = Model('tsp')
-
-    A=m.binary_var_dict(arcs,name='A')
-    S=m.continuous_var_dict(sommets,name='S')
-
-    m.minimize(m.sum(distances[i]*A[i] for i in arcs))
 
     #constraints to ensure passing exactly once by each vertex
 
-    for s in sommets:
-        m.add_constraint(m.sum(A[(i,j)] for i,j in arcs if i==s)==1)
 
-    for s in sommets:
-        m.add_constraint(m.sum(A[(i,j)] for i,j in arcs if j==s)==1)
+    N = 5
+    c =  [
+    [99, 7, 8, 5, 3],
+    [ 7,99, 2, 9,10],
+    [ 8, 2,99, 6, 9],
+    [ 5, 9, 6,99, 4],
+    [ 3,10, 9, 4,99]
+    ]
+    # Variables
+    x = m.binary_var_dict((i, j) for i in range(1, N+1) for j in range(1, N+1))
+    print(x)
+
+    nodes = list(range(N))
+    p = reduce(lambda x, y: x+y, [list(combinations(nodes, i)) for i in range(2, N)])
+    print(p)
+
+    number_of_sets = 2^N - 2
+
+    for k in range(1, number_of_sets+1):
+        if len(p[k-1])<=N-1 and len(p[k-1])>=2:
+            m.add_constraint(m.sum([x[i+1, j+1] for i in p[k-1] for j in p[k-1]])<=len(p[k])-1)
+            
+
+    m.add_constraint(m.sum([x[i, j] for j in range(1, N+1) for i in range(1, N+1)]) == 1)
+
+# constraint #2: salesperson leaving a node i
+
+    m.add_constraint(m.sum([x[i, j] for i in range(1, N+1) for j in range(1, N+1)]) == 1)
 
 
+    number_of_sets = 2^N - 2
+
+    for k in range(1, number_of_sets+1):
+        if len(p[k-1])<=N-1 and len(p[k-1])>=2:
+            m.add_constraint(m.sum([x[i+1, j+1] for i in p[k-1] for j in p[k-1]])<=len(p[k])-1)
+            
     card = (2**nb_villes)
 
     def powerset(iterable):
         s = list(iterable)
-        return chain.from_iterable(combinations(s, r) for r in range(len(s)))
+        return chain.from_iterable(combinations(s, r) for r in range(2, len(s)))
 
     subtours = list(powerset(range(nb_villes)))
 
@@ -80,12 +70,14 @@ def solve_tsp():
     #subtours = [k for k in range_set for i in range(nb_villes) if (k/(2**(i-1))) % 2 == 1]
 
 
+    '''
     for s in subtours:
         m.add_constraint(m.sum(A[(i,j)] for i,j in arcs if i!=j)<= len(s)-1)
+    '''
 
-    solution = m.solve(log_output=False)
+    #solution = m.solve(log_output=False)
     
-    print(m.get_solve_status())
+    #print(m.get_solve_status())
     #solution.display()
 
     
@@ -93,5 +85,11 @@ def solve_tsp():
     #print(subtours)
     #constraint for subtours
     # m.add_constraint()
+
+    m.minimize(sum([c[i-1][j-1]*x[i, j] for i in range(1, N+1) for j in range(1, N+1)]))
+
+    m.print_information()
+    sol = m.solve()
+    m.print_solution()
 
 solve_tsp()
